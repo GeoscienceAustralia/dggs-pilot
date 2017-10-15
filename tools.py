62e85a98d60187bef0b8fc7899d0fb91d43b1617
Kirill Kouzoubov <kirill888@gmail.com>
2017-10-15 15:12:10 +1100

    Add population data notebook

--- tools.py
+++ tools.py
@@ -32,3 +32,41 @@ def apply_affine(A, x, y):
 
     x, y = A*np.vstack([x.ravel(), y.ravel()])
     return x.reshape(sh), y.reshape(sh)
+
+
+def spread_num_samples(v):
+    v_int = np.floor(v).astype('uint32')
+    n_partials = int(v.sum() - v_int.sum())
+
+    a = np.fmod(v, 1)
+    a /= a.sum()
+
+    ii = np.random.choice(v.shape[0], n_partials, p=a, replace=False)
+    v_int[ii] += 1
+
+    return v_int
+
+
+def gen_pts_from_distribution(im, affine, noshuffle=False):
+    iy, ix = np.where(im > 0)
+
+    vv = spread_num_samples(im[iy, ix])
+
+    n_total = vv.sum()
+    off = 0
+    xy = np.zeros((2, n_total), dtype='float32')
+
+    for v, x, y in zip(vv, ix, iy):
+        if v == 0:
+            continue
+
+        pts = np.random.uniform(0, 1, size=(2, v)) + np.c_[[x, y]]
+
+        xy[:, off:off+v] = affine*pts
+
+        off += v
+
+    if noshuffle:
+        return xy
+
+    return xy[:, np.random.choice(n_total, n_total, replace=False)]
