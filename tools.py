06eee27f31c548b71799b2c0b73ce9f3aec661b8
Kirill Kouzoubov <kirill888@gmail.com>
2017-10-31 22:55:12 +1100

    Add load for hdf5 dggs files

--- tools.py
+++ tools.py
@@ -1,4 +1,6 @@
 import numpy as np
+import xarray as xr
+import h5py
 
 
 def polygon_path(x, y=None):
@@ -74,7 +76,6 @@ def gen_pts_from_distribution(im, affine, noshuffle=False):
 
 def convert_global(src_file, dst_file, scale_level, band_names=None, combine_bands=False):
     import rasterio
-    import h5py
     from .dggs import DGGS
 
     if isinstance(band_names, str):
@@ -99,6 +100,7 @@ def convert_global(src_file, dst_file, scale_level, band_names=None, combine_ban
     num_bands = len(bands)
 
     print('CRS:', src_crs)
+    print('Affine:', affine)
 
     dg = DGGS()
 
@@ -135,7 +137,6 @@ def convert_global(src_file, dst_file, scale_level, band_names=None, combine_ban
 
 def convert(src_file, dst_file, scale_level, band_names=None, inter=None):
     import rasterio
-    import h5py
     from .dggs import DGGS
 
     if isinstance(band_names, str):
@@ -233,3 +234,68 @@ def convert(src_file, dst_file, scale_level, band_names=None, inter=None):
         f.close()
 
     return cells
+
+
+def _h5_parse_structure(f):
+    sites = {}
+    bands = []
+
+    def valid_address(addr: str):
+        code, *digits = addr
+        return (code in 'SNOPQR' and
+                set(digits).issubset('012345678'))  # TODO: assumes 3x3 dggs
+
+    def add(addr, shape):
+        v = sites.get(addr)
+        if v and v != shape:
+            raise RuntimeError('Incompatible shapes detected')
+        else:
+            sites[addr] = shape
+
+    for band, g in f.items():
+        if not isinstance(g, h5py.Group):
+            continue
+
+        count = 0
+
+        for addr, ds in g.items():
+            if valid_address(addr) and isinstance(ds, h5py.Dataset):
+                add(addr, ds.shape)
+                count += 1
+
+        if count > 0:
+            bands.append(band)
+
+    return bands, sites
+
+
+def h5_load(fname, bands=None):
+
+    def read_bands(f, addr, shape, bands):
+        def read(band):
+            path = band + '/' + addr
+            ds = f.get(path)
+            if ds is None:
+                # TODO: need to know dtype and nodata value for the band
+                raise NotImplementedError("Currently only support homogeneous data across bands")
+            else:
+                if ds.shape != shape:
+                    raise NotImplementedError("Currently only support homogeneous data across bands")
+
+                dd = np.empty(shape, dtype=ds.dtype)
+                ds.read_direct(dd)
+                return dd
+
+        dims = ('y', 'x')
+        return xr.Dataset({band: (dims, read(band)) for band in bands})
+
+    with h5py.File(fname, 'r') as f:
+        bands_, sites = _h5_parse_structure(f)
+        if bands is None:
+            bands = bands_
+        else:
+            # TODO: verify that requested bands are present in a file
+            pass
+
+        return {addr: read_bands(f, addr, shape, bands)
+                for (addr, shape) in sites.items()}
